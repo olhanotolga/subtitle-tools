3b5abe2354a65a17309abfe1b98cc8645e258162
Olha Halat <olha.halat@gmail.com>
2020-08-02 00:17:35 +0200

    add update Space Remover with: 'all good' message, trailing spaces removal, 'fixed' message keeping count when spaces from just one category were fixed

--- space-remover/space-remover.js
+++ space-remover/space-remover.js
@@ -2,6 +2,8 @@ const inputField = document.getElementById("uncheckedInput");
 const outputField = document.getElementById("checkedOutput");
 const multipleSpaces = /  +/g;
 const unnecessarySpaces = / (\.)| (,)| (:)| (;)/g;
+const trailingSpacesStart = /^ +/gm;
+const trailingSpacesEnd = / +$/gm;
 const processBtn = document.querySelector("button");
 const comment = document.getElementById("comment");
 
@@ -9,15 +11,45 @@ processBtn.addEventListener("click", function(){
   let input = inputField.value;
   // fix repeating spaces:
   let output = input.replace(multipleSpaces, " ");
+  
   // fix spaces before commas or periods or colons or semicolons:
   output = output.replace(unnecessarySpaces, "$1$2$3$4");
+
+  // fix trailing spaces:
+  output = output.replace(trailingSpacesStart, "");
+  output = output.replace(trailingSpacesEnd, "");
+
   // display output:
   outputField.value = output;
   // display information on errors in the input:
-  let multipleLength = input.match(multipleSpaces).length;
-  let unnecessaryLength = input.match(unnecessarySpaces).length;
+  let multipleLength;
+  let unnecessaryLength;
+  let trailingSpacesStartLength;
+  let trailingSpacesEndLength;
+
+  if (!multipleSpaces.test(input)) {
+    multipleLength = 0;
+  } else {
+    multipleLength = input.match(multipleSpaces).length;
+  }
+  if (!unnecessarySpaces.test(input)) {
+    unnecessaryLength = 0;
+  } else {
+    unnecessaryLength = input.match(unnecessarySpaces).length;
+  }
+  if (!trailingSpacesStart.test(input)) {
+    trailingSpacesStartLength = 0;
+  } else {
+    trailingSpacesStartLength = input.match(trailingSpacesStart).length;
+  }
+  if (!trailingSpacesEnd.test(input)) {
+    trailingSpacesEndLength = 0;
+  } else {
+    trailingSpacesEndLength = input.match(trailingSpacesEnd).length;
+  }
+  
   if (multipleLength || unnecessaryLength) {
-    let info = multipleLength + unnecessaryLength;
+    let info = multipleLength + unnecessaryLength + trailingSpacesStartLength + trailingSpacesEndLength;
     comment.textContent = `Fixed: ${info} instances of redundant spaces.`;
   } else {
     comment.textContent = `No critically redundant spaces found.`;
